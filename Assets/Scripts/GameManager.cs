51d5ef2e4fc2b25e2e9abdcaf808d2f145bd478c
Doğaç Mengül <dogacmengul@yahoo.com>
2024-04-03 16:14:28 +0300

    Main Menu Implementation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,8 +3,19 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    public void LoadGame()
+    public void StartGame()
     {
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
         SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
--- Assets/Scripts/OptionsUI.cs
+++ Assets/Scripts/OptionsUI.cs
@@ -1,14 +1,10 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.Serialization;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class OptionsUI : MonoBehaviour
 {
-    [SerializeField] private Button _sFXDecreaseButton;
-    [SerializeField] private Button _sFXIncreaseButton;
-    [SerializeField] private Button _musicDecreaseButton;
-    [SerializeField] private Button _musicIncreaseButton;
     [SerializeField] private Button _mainMenuButton;
     [SerializeField] private TextMeshProUGUI _sFXVolumeText;
     [SerializeField] private TextMeshProUGUI _musicVolumeText;
@@ -16,12 +12,26 @@ public class OptionsUI : MonoBehaviour
 
     private void Awake()
     {
-        //gameObject.SetActive(false);
+        gameObject.SetActive(false);
     }
 
     private void Start()
     {
         SetVolumeText();
+    }
+    
+    public void ToggleOptionsUI()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+
+        if (gameObject.activeInHierarchy)
+        {
+            Time.timeScale = 0f; // Pause game
+        }
+        else
+        {
+            Time.timeScale = 1f; // Resume game
+        }
     }
 
     public void DecreaseSFX()
@@ -53,4 +63,5 @@ public class OptionsUI : MonoBehaviour
         _sFXVolumeText.text = Mathf.RoundToInt(AudioManager.Instance.SFXVolume * 10).ToString();
         _musicVolumeText.text = Mathf.RoundToInt(AudioManager.Instance.MusicVolume * 10).ToString();
     }
+    
 }
